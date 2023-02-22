a1c97b9df17870fc10c53466ecdf0a3d5b1c923c
yybgx <2398456592qq.com>
2023-02-23 00:03:25 +0800

    相关文章修改

--- pages/news_detail.vue
+++ pages/news_detail.vue
@@ -66,6 +66,55 @@
         {{ newsdetail.detail}}
       </div>
     </div>
+    <div class="right">
+      <div class="code">
+        <div class="head_tit">
+          <img
+            :src="avatar"
+            loading="lazy"
+            class="img_head"
+          />
+          <div class="head_name">
+            <p>
+              {{this.authorname}}<span
+                ><img
+                  src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png"
+                  style="width: 35px; height: 16px"
+              /></span>
+            </p>
+            <p>{{ introduce }}</p>
+          </div>
+        </div>
+        <el-row>
+          <el-button type="primary">关注</el-button>
+          <el-button type="primary" plain>私信</el-button>
+        </el-row>
+        <hr class="hr1" />
+        <p class="cont_p">
+          <span class="img_good"></span><span class="good">当前年度排名</span
+          ><span class="good">1{{ranking}}</span>
+        </p>
+        <p class="cont_p">
+          <span class="img_good">111</span><span class="good">获得点赞</span
+          ><span class="good">11426</span>
+        </p>
+      </div>
+      <img src="@/assets/images/11.png" class="img_code" />
+      <img src="@/assets/images/12.png" class="img_code" />
+
+      <div class="other">
+        <div class="other_tit" style="text-align: center;">相关文章</div>
+        <hr class="hr1" />
+        <div class="other_td"  v-for="(news,index) in newslist" :key="index" v-if="index<5" v-on:click="todeatil(news.id)">
+          <span>{{ news.Newsname }}</span>
+          <!-- <span class="other_tt">|</span><span>{{news.auhors[0].name}}</span> -->
+          <div class="other_good">30点赞 25评论</div>
+        </div>
+      </div>
+      <div class="clist" style="text-align: center;">
+        目录
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -74,6 +123,8 @@ import Cookie from "js-cookie";
 export default {
   data() {
     return {
+      ranking:'',
+      introduce:'',
       avatar: "",
       authorname: "",
       news_id: "",
@@ -81,6 +132,7 @@ export default {
       isture: true,
       isadd: false,
       role: "",
+      newslist:[],
       newsdetail: [],
       comments: [{}],
     };
@@ -95,17 +147,115 @@ export default {
       console.log(result);
       this.newsdetail = result.data;
       this.authorname = result.data.auhors[0].name;
+      this.introduce=result.data.auhors[0].Introduce;
+      this.ranking=result.data.auhors[0].Ranking;
       this.avatar =
         "http://localhost:1337" + result.data.auhors[0].avatar[0].url;
       // this.imageurl='http://localhost:1337'+result.data[0].image[0].url;
       console.log(this.newsdetail);
     });
+    this.$axios({
+        method:'get',
+        url:'http://localhost:1337/newslists/',
+      }).then((result)=>{
+        console.log(result);
+        this.newslist=result.data;
+        // this.imageurl='http://localhost:1337'+result.data[0].image[0].url;
+      })
   },
+  
 
 };
 </script>
 
 <style>
+.other {
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0.1rem 1rem rgb(222, 218, 218);
+  padding: 1rem;
+  margin-top: 1rem;
+}
+.other_tit {
+  margin-bottom: 1rem;
+}
+.other_cont {
+  margin-top: 1rem;
+}
+.other_td {
+  margin-top: 1rem;
+}
+
+.other_tt {
+  margin: 0 1.5rem;
+}
+.other_good {
+  color: rgb(102, 101, 101);
+  margin-top: 0.2rem;
+}
+.clist{
+  margin-top: 1rem;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0.1rem 1rem rgb(222, 218, 218);
+  padding: 1rem;
+  margin-bottom: 2rem;
+}
+
+.img_code {
+  width: 21rem;
+  margin-top: 1.8rem;
+}
+.img_good {
+  display: inline-block;
+  width: 1.5rem;
+  height: 1.5rem;
+  background-color: red;
+  margin-left: 1.5rem;
+}
+.good {
+  margin-left: 1.1rem;
+  display: inline-block;
+}
+.right {
+  float: right;
+  margin: 1rem 10rem 1rem 0rem;
+  width: 19rem;
+  height: 29rem;
+}
+.code {
+  margin-top: 1rem;
+  width: 100%;
+  height: 15rem;
+  box-shadow: 0 0.1rem 1rem rgb(222, 218, 218);
+  padding: 1rem;
+}
+.head_tit {
+  display: inline-block;
+}
+.head_name {
+  float: right;
+  position: relative;
+  top: 0.8rem;
+}
+.head_name p {
+  margin: 0.1rem 0 0;
+}
+.el-button {
+  width: 8rem;
+  margin: 0.8rem 0 1rem 1rem;
+}
+.hr1 {
+  border: none;
+  height: 1px;
+  background-color: rgb(240, 241, 243);
+}
+.img_head {
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  margin: 1rem;
+}
 .content {
   width: 90%;
   font-size: 1rem;
@@ -188,7 +338,7 @@ export default {
 .detail {
   display: inline-block;
   margin-left: 2vw;
-  margin-top: 1rem;
+  margin-top: 2rem;
   height:100%;
   width: 52.5vw;
   background-color: rgb(255, 255, 255);
